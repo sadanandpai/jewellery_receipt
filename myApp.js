53f3f7eb40137313b685e806a5d278b8a76937d2
Akshay Pai <sadypai@gmail.com>
2016-12-30 23:24:08 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -5,9 +5,9 @@ angular.module('ItemApp', [])
  
     itemList.additem = function() {
       itemList.items.push({item:itemList.item, qty:itemList.qty, rate:itemList.rate, done:false});
-      itemList.item = ''; 
-      itemList.qty = ''; 
-      itemList.rate = ''; 
+      itemList.item = '';
+      itemList.qty = '';
+      itemList.rate = '';
     };
  
     itemList.deleteItem = function() {
