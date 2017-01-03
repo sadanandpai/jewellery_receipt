b9e84bbe4734fce1d72d516c606bda7c7090d7df
Akshay Pai <sadypai@gmail.com>
2017-01-03 21:57:18 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,20 +1,70 @@
 angular.module('ItemApp', [])
-  .controller('ItemListController', function() {
-    var itemList = this;
-    itemList.items = [];
+  .controller('ItemListController', function($scope) {
+    $scope.itemList = [
+      {name:'aaa', qty:'11', rate:'1212'},
+      {name:'bbb', qty:'22', rate:'3434'},
+      {name:'ccc', qty:'33', rate:'6767'}
+    ];
+    $scope.selectionCounter = 0;
+    $scope.singleSelection = false;
  
-    itemList.additem = function() {
-      itemList.items.push({item:itemList.item, qty:itemList.qty, rate:itemList.rate, done:false});
-      itemList.item = '';
-      itemList.qty = '';
-      itemList.rate = '';
+    $scope.additem = function() {
+      $scope.itemList.push({name:$scope.itemName, qty:$scope.itemQty, rate:$scope.itemRate, done:false});
+      $scope.itemName = '';
+      $scope.itemQty = '';
+      $scope.itemRate = '';
     };
  
-    itemList.deleteItem = function() {
-      var olditems = itemList.items;
-      itemList.items = [];
+    $scope.deleteItem = function() {
+      var olditems = $scope.itemList;
+      $scope.itemList = [];
       angular.forEach(olditems, function(item) {
-        if (!item.done) itemList.items.push(item);
+        if (!item.done) $scope.itemList.push(item);
       });
     };
+
+    $scope.editItem = function() {
+      if($scope.selectionCounter > 1){
+          $scope.itemName ='';
+          $scope.itemQty = '';
+          $scope.itemRate ='';
+      }
+      else{
+          angular.forEach($scope.itemList, function(item) {
+          if (item.done){
+            $scope.itemName = item.name;
+            $scope.itemQty = item.qty;
+            $scope.itemRate = item.rate;
+          }
+        });
+      }
+    };
+
+
+    $scope.updateItem = function() {
+      angular.forEach($scope.itemList, function(item) {
+        if ($scope.selectionCounter == 1 && item.done){
+          item.name = $scope.itemName;
+          item.qty = $scope.itemQty ;
+          item.rate = $scope.itemRate;
+          $scope.itemName ='';
+          $scope.itemQty = '';
+          $scope.itemRate ='';
+        }
+      });
+    };
+
+
+    $scope.selectionCount = function() {
+      $scope.selectionCounter = 0;
+      angular.forEach($scope.itemList, function(item) {
+        if (item.done)
+          $scope.selectionCounter++;
+      });
+      if($scope.selectionCounter == 1)
+        $scope.singleSelection = true;
+      else
+        $scope.singleSelection = false;
+    };
+
   });
